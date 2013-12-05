3c7e592f8ef7bce81c9564c7187ea4a2485adc13
Bo Liu <wy90021@gmail.com>
2013-12-05 12:34:59 -0800

    add deviceUri to Sensor

--- app/models/Sensor.java
+++ app/models/Sensor.java
@@ -4,6 +4,7 @@ package models;
 public class Sensor extends SensorType{
 	private String sensorName;
 	private String sensorUserDefinedFields;
+	private String deviceUri;
 	
 	public String getSensorName() {
 		return sensorName;
@@ -17,25 +18,11 @@ public class Sensor extends SensorType{
 	public void setSensorUserDefinedFields(String userDefinedFields) {
 		this.sensorUserDefinedFields = userDefinedFields;
 	}
+	public String getDeviceUri() {
+		return deviceUri;
+	}
+	public void setDeviceUri(String deviceUri) {
+		this.deviceUri = deviceUri;
+	}
 	
-//	public String toJSONString() {
-//		String sensorTypeJsonString = super.toJSONString();
-//		String jsonString = new String();
-//		try {
-//			JSONObject sensorTypeJsonObj = new JSONObject(sensorTypeJsonString);
-//			
-//			JSONObject obj=new JSONObject();
-//			obj.put("sensor_name",  sensorName);
-//			obj.put("user_defined_fields", sensorUserDefinedFields);
-//			Iterator it = sensorTypeJsonObj.keys();
-//			while (it.hasNext()) {
-//				String key = (String)it.next();
-//				obj.put(key, sensorTypeJsonObj.get(key));
-//			}
-//			jsonString = obj.toString();
-//		} catch (JSONException e) {
-//			e.printStackTrace();
-//		}
-//		return jsonString;
-//	}
 }
