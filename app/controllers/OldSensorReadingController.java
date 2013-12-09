7927b76e92641917fbcc6d9bd4348c3f12013d33
Bo Liu <wy90021@gmail.com>
2013-12-08 19:44:20 -0800

    add license

--- app/controllers/OldSensorReadingController.java
+++ app/controllers/OldSensorReadingController.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Carnegie Mellon University Silicon Valley. 
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available
+ * under the terms of dual licensing(GPL V2 for Research/Education
+ * purposes). GNU Public License v2.0 which accompanies this distribution
+ * is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * 
+ * Please contact http://www.cmu.edu/silicon-valley/ if you have any 
+ * questions.
+ ******************************************************************************/
 package controllers;
 
 import helper.Utils;
