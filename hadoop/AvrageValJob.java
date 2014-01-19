0241f4d1b52365667a070f7eca0711e039623f7c
innovateboliu <wy90021@gmail.com>
2014-01-18 16:35:31 -0800

    add Combiner

--- hadoop/AvrageValJob.java
+++ hadoop/AvrageValJob.java
@@ -1,4 +1,7 @@
 package hadoop;
+
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
@@ -13,17 +16,55 @@ import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 
 
-public class AvrageValJob {
+public class AverageValJob {
+	
+	public class SumCountWritable implements Writable {
 
-	public static class AverageMapper extends TableMapper<Text, DoubleWritable> {
+		private double sum;
+		private int count;
+		
+		
+		public double getSum() {
+			return sum;
+		}
+
+		public void setSum(double sum) {
+			this.sum = sum;
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		public void setCount(int count) {
+			this.count = count;
+		}
+
+		@Override
+		public void readFields(DataInput in) throws IOException {
+			sum = in.readDouble();
+			count = in.readInt();
+		}
+
+		@Override
+		public void write(DataOutput out) throws IOException {
+			out.writeDouble(sum);
+			out.writeInt(count);
+		}
+		
+	}
+
+	public static class AverageMapper extends TableMapper<Text, SumCountWritable> {
 		private static final String regex = "sid|ts";
-		private static final long start = 1385938651;
-		private static final long end = 1388617051;
+		private static final long start = 1385938651;  //Dec 01, 2013
+		private static final long end = 1388617051;    //Jan 01, 2014
 		private static final byte[] CF = "value".getBytes();
 		private static final byte[] QF = "numerical".getBytes();
+		private SumCountWritable sumCount = new AverageValJob().new SumCountWritable();
 		
 		public void map(ImmutableBytesWritable row, Result value, Context context) throws IOException, InterruptedException {
 			String[] sidAndTs = new String(row.get()).split(regex);
@@ -34,29 +75,47 @@ public class AvrageValJob {
 				byte[] valueBytes = value.getValue(CF, QF);
 				if (valueBytes != null) {
 					String str = new String(valueBytes);
-					context.write(new Text(sid), new DoubleWritable(Double.parseDouble(str)));
+					sumCount.setCount(1);
+					sumCount.setSum(Double.parseDouble(str));
+					context.write(new Text(sid), sumCount);
 				}
 			}
 		}
 	}
 	
-	public static class AverageReducer extends TableReducer<Text, DoubleWritable, ImmutableBytesWritable> {
+	public static class AverageCombiner extends TableReducer<Text, SumCountWritable, Text> {
+		@Override
+		public void reduce(Text key, Iterable<SumCountWritable> values, Context context) throws IOException, InterruptedException{
+			double sum = 0;
+			int count = 0;
+			for (SumCountWritable value : values) {
+				sum += value.getSum();
+				count += value.getCount();
+			}
+			
+			SumCountWritable sumCount = new AverageValJob().new SumCountWritable();
+			sumCount.setCount(count);
+			sumCount.setSum(sum);
+			
+			context.write(key, sumCount);
+		}
+	} 
+	
+	public static class AverageReducer extends TableReducer<Text, SumCountWritable, ImmutableBytesWritable> {
 		public static final byte[] CF = "month".getBytes();
 		public static final byte[] QF = "averageValue".getBytes();
 		
-		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<SumCountWritable> values, Context context) throws IOException, InterruptedException {
 			double sum = 0;
-			double num = 0;
-			for (DoubleWritable val : values) {
-				sum += val.get();
-				num += 1;
+			double count = 0;
+			for (SumCountWritable val : values) {
+				sum += val.getSum();
+				count += val.getCount();
 			}
-			System.out.println("new rowkey: " + key.toString());
+			
 			Put put = new Put(Bytes.toBytes(key.toString()));
 			
-			System.out.println("new value: " + sum / num);
-
-			put.add(CF, QF, Bytes.toBytes(String.valueOf(sum / num)));
+			put.add(CF, QF, Bytes.toBytes(String.valueOf(sum / count)));
 			
 			context.write(null, put);
 		}
@@ -65,11 +124,11 @@ public class AvrageValJob {
 	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 		Configuration config = HBaseConfiguration.create();
 		Job job = new Job(config,"ExampleSummary");
-		job.setJarByClass(AvrageValJob.class);    
+		job.setJarByClass(AverageValJob.class);    
 
 		Scan scan = new Scan();
 		scan.setCaching(500);        
-		scan.setCacheBlocks(false);  
+		scan.setCacheBlocks(false);   
 
 		TableMapReduceUtil.initTableMapperJob(
 			"SensorReading",        
