ef7c56023f2de42cd43f3a0afdee146c2344b4c3
Lyman Cao <caoyifanlyman@gmail.com>
2014-02-23 21:56:26 -0800

    reset sensor category to default when sensor category is deleted

--- app/models/dao/SensorCategoryDaoImplementation.java
+++ app/models/dao/SensorCategoryDaoImplementation.java
@@ -22,37 +22,39 @@ import models.SensorCategory;
 import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
-public class SensorCategoryDaoImplementation implements SensorCategoryDao{
+public class SensorCategoryDaoImplementation implements SensorCategoryDao {
 	private SimpleJdbcTemplate simpleJdbcTemplate;
-	
+
 	@Override
 	public boolean addSensorCategory(String sensorCategoryName, String purpose) {
 		// TODO need to use this in production for SAP HANA
 		final String SQL_SEQUENCE = "SELECT CMU.COURSE_SENSOR_CATEGORY_ID_SEQ.NEXTVAL FROM DUMMY";
 		int sensorCategoryId = simpleJdbcTemplate.queryForInt(SQL_SEQUENCE);
-		
+
 		// TODO need to use this in production for SAP HANA
 		final String SQL = "INSERT INTO CMU.COURSE_SENSOR_CATEGORY (SENSOR_CATEGORY_ID, SENSOR_CATEGORY_NAME, PURPOSE) VALUES (?, ?, ?)";
-		
+
 		// for test only
-//		final String SQL = "INSERT INTO CMU.COURSE_SENSOR_CATEGORY (SENSOR_CATEGORY_ID, SENSOR_CATEGORY_NAME, PURPOSE) VALUES (next value for CMU.COURSE_SENSOR_CATEGORY_ID_SEQ, ?, ?)";
-		try{
+		// final String SQL =
+		// "INSERT INTO CMU.COURSE_SENSOR_CATEGORY (SENSOR_CATEGORY_ID, SENSOR_CATEGORY_NAME, PURPOSE) VALUES (next value for CMU.COURSE_SENSOR_CATEGORY_ID_SEQ, ?, ?)";
+		try {
 			// TODO need to use this in production for SAP HANA
-			simpleJdbcTemplate.update(SQL, sensorCategoryId, sensorCategoryName, purpose);
+			simpleJdbcTemplate.update(SQL, sensorCategoryId,
+					sensorCategoryName, purpose);
 			// for test only
-//			simpleJdbcTemplate.update(SQL, sensorCategoryName, purpose);
+			// simpleJdbcTemplate.update(SQL, sensorCategoryName, purpose);
 		} catch (Exception e) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	@Override
-	public boolean updateSensorCategory(String sensorCategoryName, String purpose) {
-		final String SQL = "UPDATE CMU.COURSE_SENSOR_CATEGORY " 
-				+ "SET PURPOSE = ? "
-				+ "WHERE SENSOR_CATEGORY_NAME = ?";
-		try{
+	public boolean updateSensorCategory(String sensorCategoryName,
+			String purpose) {
+		final String SQL = "UPDATE CMU.COURSE_SENSOR_CATEGORY "
+				+ "SET PURPOSE = ? " + "WHERE SENSOR_CATEGORY_NAME = ?";
+		try {
 			simpleJdbcTemplate.update(SQL, purpose, sensorCategoryName);
 		} catch (Exception e) {
 			return false;
@@ -63,10 +65,12 @@ public class SensorCategoryDaoImplementation implements SensorCategoryDao{
 	@Override
 	public List<SensorCategory> getAllSensorCategories() {
 		final String SQL = "SELECT * FROM CMU.COURSE_SENSOR_CATEGORY";
-		try{
-			List<SensorCategory> sensorCategories = simpleJdbcTemplate.query(SQL, ParameterizedBeanPropertyRowMapper.newInstance(SensorCategory.class));
+		try {
+			List<SensorCategory> sensorCategories = simpleJdbcTemplate.query(
+					SQL, ParameterizedBeanPropertyRowMapper
+							.newInstance(SensorCategory.class));
 			return sensorCategories;
-		}catch(Exception e){
+		} catch (Exception e) {
 			return null;
 		}
 	}
@@ -74,21 +78,33 @@ public class SensorCategoryDaoImplementation implements SensorCategoryDao{
 	@Override
 	public SensorCategory getSensorCategory(String SensorCategoryName) {
 		final String SQL = "SELECT * FROM CMU.COURSE_SENSOR_CATEGORY WHERE SENSOR_CATEGORY_NAME = ?";
-		try{
-			SensorCategory sensorCategory = simpleJdbcTemplate.queryForObject(SQL, ParameterizedBeanPropertyRowMapper.newInstance(SensorCategory.class), SensorCategoryName);
+		try {
+			SensorCategory sensorCategory = simpleJdbcTemplate.queryForObject(
+					SQL, ParameterizedBeanPropertyRowMapper
+							.newInstance(SensorCategory.class),
+					SensorCategoryName);
 			return sensorCategory;
-		}catch(Exception e){
+		} catch (Exception e) {
 			return null;
 		}
 	}
-	
+
 	@Override
 	public boolean deleteSensorCategory(String sensorCategoryName) {
+		// change to default sensor category
+		final String SQL_RESET_SENSOR_TYPES = "UPDATE FROM CMU.COURSE_SENSOR_TYPE "
+				+ "WHERE SENSOR_CATEGORY_ID = "
+				+ "(SELECT SENSOR_CATEGORY_ID FROM CMU.COURSE_SENSOR_CATEGORY "
+				+ "WHERE SENSOR_CATEGORY_NAME = ?) "
+				+ "SET SENSOR_CATEGORY_ID = 0";
 		final String SQL_DELETE_SENSOR_CATEGORY = "DELETE FROM CMU.COURSE_SENSOR_CATEGORY "
-						+ "WHERE SENSOR_CATEGORY_NAME = ?";
-		try{
-			simpleJdbcTemplate.update(SQL_DELETE_SENSOR_CATEGORY, sensorCategoryName);
-		}catch(Exception e){
+				+ "WHERE SENSOR_CATEGORY_NAME = ?";
+		try {
+			simpleJdbcTemplate.update(SQL_RESET_SENSOR_TYPES,
+					sensorCategoryName);
+			simpleJdbcTemplate.update(SQL_DELETE_SENSOR_CATEGORY,
+					sensorCategoryName);
+		} catch (Exception e) {
 			return false;
 		}
 		return true;
