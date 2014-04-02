f2716424446d453497bc395b4d12a109f35fc3e8
innovateboliu <wy90021@gmail.com>
2014-04-02 16:48:30 -0700

    debug

--- app/models/dao/SensorReadingDaoImplementation.java
+++ app/models/dao/SensorReadingDaoImplementation.java
@@ -44,6 +44,11 @@ public class SensorReadingDaoImplementation implements SensorReadingDao{
 	
 	@Override
 	public SensorReading searchReading(String deviceUri, String sensorTypeName, Long timeStamp) {
+		System.out.println("deviceUri is " + deviceUri);
+		System.out.println("sensorTypeName is " + sensorTypeName);
+
+		
+
 		final String SQL = "SELECT * FROM CMU.COURSE_DISCRETE_SENSOR_READING DSR, CMU.COURSE_SENSOR S WHERE DSR.SENSOR_ID=S.SENSOR_ID AND S.SENSOR_NAME=? AND DSR.timeStamp<=? ORDER BY timeStamp DESC LIMIT 1";
 		final String GET_SENSOR_NAME = "SELECT S.SENSOR_NAME FROM CMU.COURSE_DEVICE D, CMU.COURSE_SENSOR S, CMU.COURSE_SENSOR_TYPE ST WHERE D.DEVICE_ID=S.DEVICE_ID AND S.SENSOR_TYPE_ID=ST.SENSOR_TYPE_ID AND D.URI=? AND ST.SENSOR_TYPE_NAME=?";
 		try {
